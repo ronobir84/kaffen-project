bab007318022bee8476b49bde4ede38476e26543
ronodir84 <priashdas@84gmail.com>
2024-01-16 14:38:06 +0600

    add the banner responsive and finshed

--- src/Pages/HomePage/Banner/Banner.jsx
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -1,13 +1,58 @@
- import img1 from"../../../assets/image/banner1.jpg"
-
+import { useState } from "react";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+ 
 const Banner = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const prevSlide = () => {
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex)
+    }
+    const nextSlide = () => {
+        const isLastSlide = currentIndex === slides.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex)
+    }
+    const slides =[
+        {
+            url: "https://i.ibb.co/kmB3qpZ/banner1.jpg",
+             text : "hello"
+        },
+        {
+            url: "https://i.ibb.co/Wnv2KPq/banner2.jpg",
+        },
+        {
+            url: "https://i.ibb.co/g3ycmKq/banner3.jpg"
+        }
+     ]
     return (
-        <div>
-            <div>
-                <img src={img1} alt="" />
+        <div className=" md:min-h-screen   py-16  relative group ">
+            <div style={{ backgroundImage: `url(${slides[currentIndex].url})` }} className="md:w-full  md:min-h-screen  h-[400px] bg-cover duration-700">
+                <div className="absolute top-[30%] md:left-[10%] md:space-y-5 ">
+                    <p className="md:text-2xl  text-white font-bold uppercase">Welcome to the kaffen</p>
+                    <h2 className="text-white md:text-[90px] md:font-extrabold text-5xl font-bold ">The Paris <br /> Coffee  House</h2>
+                    <div className="">
+                        <a href="#" className="custom-btn btn-14 relative  top-6 ">explore more</a>
+                        <a href="#" className="custom-btn btn-15 relative left-5  top-6 ">get delivery</a>
+                         
+                    </div>
+                </div>
             </div>
+            
+            {/* left arrow */}
+            <div className="   absolute md:top-[50%] top-[80%] -translate-x-0 translate-y-[-50%]   text-2xl    bg-[#FFFFFF] text-[#B99272] hover:text-white hover:bg-[#B99272] md:w-20 md:h-20 w-14 h-14 hover:duration-700 cursor-pointer  rounded-e-full">
+                <FaAngleLeft onClick={prevSlide} className="md:text-4xl text-3xl relative md:top-5 top-3 left-3" ></FaAngleLeft>
+            </div>
+
+            {/* right arrow */}
+            <div className=" absolute md:top-[50%] top-[80%] -translate-x-0 translate-y-[-50%] right-0 text-2xl  bg-[#FFFFFF] text-[#B99272] hover:text-white hover:bg-[#B99272] md:w-20 md:h-20 w-14 h-14 hover:duration-700 cursor-pointer  rounded-s-full">
+                <FaAngleRight onClick={nextSlide} className="md:text-4xl text-3xl relative md:top-5 top-3 left-4 md:left-6"></FaAngleRight>
+            </div>
+           
+
+            {/* hidden group-hover:block */}
         </div>
     );
-};
-
-export default Banner;
+ };
+ 
+ export default Banner;
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 import logo from "../../../assets/image/logo.png"
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
  
 
 const Navbar = () => {
     const navLink = <>
-        <li><Link to="/" className= "md:text-xl text-md  uppercase text-white font-semibold hover:text-[#b99272] hover:duration-700 ">Home</Link></li>
-        <li><Link  to="/" className = "md:text-xl text-md  uppercase text-white font-semibold hover:text-[#b99272] hover:duration-700">About</Link></li>
-        <li><Link  to="/" className = "md:text-xl text-md uppercase text-white font-semibold hover:text-[#b99272] hover:duration-700">Menu</Link></li>
-        <li><Link  to="/" className = "md:text-xl text-md uppercase text-white font-semibold hover:text-[#b99272] hover:duration-700">Blog</Link></li>
-        <li><Link  to="/" className = "lg:text-xl text-md uppercase text-white font-semibold hover:text-[#b99272] hover:duration-700">Contacts</Link></li>
+        <li><NavLink to="/" className= "md:text-xl text-lg  uppercase text-white font-semibold hover:text-[#b99272] hover:duration-700 ">Home</NavLink></li>
+        <li><NavLink  to="/" className = "md:text-xl text-lg  uppercase text-white font-semibold hover:text-[#b99272] hover:duration-700">About</NavLink></li>
+        <li><NavLink  to="/" className = "md:text-xl text-lg uppercase text-white font-semibold hover:text-[#b99272] hover:duration-700">Menu</NavLink></li>
+        <li><NavLink  to="/" className = "md:text-xl text-lg uppercase text-white font-semibold hover:text-[#b99272] hover:duration-700">Blog</NavLink></li>
+        <li><NavLink  to="/" className = "lg:text-xl text-lg uppercase text-white font-semibold hover:text-[#b99272] hover:duration-700">Contacts</NavLink></li>
     </>
 
     return (
@@ -20,10 +20,10 @@ const Navbar = () => {
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-10 h-10 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                         </div>
-                        <ul tabIndex={0} className=" menu-sm dropdown-content mt-3 z-[1] px-12 pt-6 space-y-3 shadow-md  bg-[#090C0F] rounded-box w-72 h-72">
+                        <ul tabIndex={0} className=" menu-sm dropdown-content mt-3 z-[1] px-12 pt-6 space-y-3 shadow-md  bg-[#090C0F] rounded-box w-80 h-[320px]">
                             {navLink}
                             <div className="block md:hidden">
-                                <a href="#" className="custom-btn btn-14 px-4  ">Book a table</a>
+                                <a href="#" className="custom-btn btn-14  ">Book a table</a>
                             </div>
                         </ul>
                     </div>
