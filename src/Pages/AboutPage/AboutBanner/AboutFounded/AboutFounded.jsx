18ba465c75f0681f6a70f3a8622e0ccba54edf7b
ronodir84 <priashdas@84gmail.com>
2024-01-20 21:46:01 +0600

    add the main add

--- src/Pages/AboutPage/AboutBanner/AboutFounded/AboutFounded.jsx
+++ src/Pages/AboutPage/AboutBanner/AboutFounded/AboutFounded.jsx
@@ -4,38 +4,38 @@ import FoundedMin from "../../../../assets/about-page-img/Founded-min.png"
 
 const AboutFounded = () => {
     return (
-        <div className="mt-16">
-            <div className="grid grid-cols-1 md:grid-cols-2">
+        <div className="mt-16 md:mx-32 py-20">
+            <div className="grid grid-cols-1 md:grid-cols-2 ">
                 {/* text section */}
-                <div>
+                <div className="space-y-6 pt-10">
                     <div>
-                        <p className="text-xl font-medium text-[#b99272]">ABOUT KAFFEN</p>
-                        <h1 className="text-4xl font-bold text-white">New London Coffee Founded For Extraordinary Test</h1>
-                        <p className="text-xl font-medium text-gray-400">Sed ut perspiciatis unde omnis iste natus error voluptate accusantium doloremque laudantium, totam rem aperiam eaque ipsa quae abillo inventore veritatis</p>
+                        <p className="text-xl font-medium text-[#b99272] ">ABOUT KAFFEN</p>
+                        <h1 className="text-4xl font-bold text-white pt-5">New London Coffee Founded For Extraordinary Test</h1>
+                        <p className="text-xl font-medium text-gray-400 pt-5">Sed ut perspiciatis unde omnis iste natus error voluptate accusantium doloremque laudantium, totam rem aperiam eaque ipsa quae abillo inventore veritatis</p>
 
                     </div>
-                    <div>
+                    <div className="pt-5">
                         {/* first */}
-                        <div>
+                        <div className="flex gap-5 items-center ">
                             <div>
-                                <img src={FoundedImg1} alt="" />
+                                <img className="w-28 " src={FoundedImg1} alt="" />
                             </div>
-                            <div>
+                            <div className="space-y-2">
                                 <h2 className="text-3xl font-semibold text-white ">Natural Coffee Beans</h2>
-                                <p className="text-xl font-medium text-gray-400">Sed ut perspiciatis unde omnis iste natus error voluptate accusantium doloremque</p>
+                                <p className="text-xl font-medium text-gray-400">Sed ut perspiciatis unde omnis iste natus error <br /> voluptate accusantium doloremque</p>
                             </div>
                         </div>
                         {/* second  */}
-                        <div>
+                        <div className="flex gap-5 items-center pt-5">
                             <div>
-                                <img src={FoundedImg2} alt="" />
+                                <img className="w-28" src={FoundedImg2} alt="" />
                             </div>
-                            <div>
+                            <div className="space-y-2">
                                 <h2 className="text-3xl font-semibold text-white">100% ISO Certification</h2>
-                                <p className="text-xl font-medium text-gray-400">Sed ut perspiciatis unde omnis iste natus error voluptate accusantium doloremque</p>
+                                <p className="text-xl font-medium text-gray-400">Sed ut perspiciatis unde omnis iste natus error <br /> voluptate accusantium doloremque</p>
                             </div>
                         </div>
-                        <div>
+                        <div className="pt-5">
                             <a href="#" className="custom-btn btn-14 relative  top-6 ">Our Home</a>
                         </div>
                     </div>
